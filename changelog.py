b43b07cf405e4cbae5df7433825851d091d07962
Balint Zoltan <balint.istvan.zoltan@gmail.com>
2024-10-31 10:14:33 +0200

    generate changelog script

--- changelog.py
+++ changelog.py
@@ -1,44 +1,75 @@
 # read all .json files from changelog/ folder, sorted asc and create a changelog.html file
 # with all the changes from the json files
-
+import json
 import os
 import re
 import datetime
+import markdown
 
-# read all json files from versions/ folder
-html_rows = []
-for file in os.listdir("changelog/html"):
-    if file.endswith(".html"):
-        with open("changelog/html/" + file, "r") as f:
-            # read html
-            data = f.read()
-            # extract content from HTML <body> tag
-            body = re.search(r'<body>(.*)</body>', data, re.DOTALL).group(1)
-            # remove <div class="title">...</div> from body
-            body = re.sub(r'<div class="title">.*</div>', "", body)
-            body = body.strip()
+def changelog():
+    html_rows = []
+    for file in os.listdir("changelog/html"):
+        if file.endswith(".html"):
+            with open("changelog/html/" + file, "r") as f:
+                # read html
+                data = f.read()
+                # extract content from HTML <body> tag
+                body = re.search(r'<body>(.*)</body>', data, re.DOTALL).group(1)
+                # remove <div class="title">...</div> from body
+                body = re.sub(r'<div class="title">.*</div>', "", body)
+                body = body.strip()
 
-            if len(body) == 0:
-                continue
+                if len(body) == 0:
+                    continue
 
-            # filename = {timestamp}-{git hash}.json
-            timestamp = file.split("-")[0]
-            git_hash = file.split("-")[1].split(".")[0]
+                # filename = {timestamp}-{git hash}.json
+                timestamp = file.split("-")[0]
+                git_hash = file.split("-")[1].split(".")[0]
 
-            # html row: date, link to git commit, changes
-            html_rows.append(f"""
-            <div>
-                <div class="title">{datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M')}</div>
-                <div class="link"><a href="#{git_hash}">View code changes [{git_hash[:8]}]</a></div>
-                {body}
-                <ul>
+                # load metadata json
+                metadata = json.load(open("changelog/metadata/" + file.replace(".html", ".metadata.json"), "r"))
+
+                # html row: date, link to git commit, changes
+                summary = dump_summary(metadata)
+                html_rows.append(f"""
+                <div class="section">
+                    <h2 class="title"><span class="dot"></span> {datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M')}</h2>
+                    {summary}
+                    <div class="separator"></div>
+                    {body}
+                </div>
+    """)
+
+    # dump to index.html file based on template.html, replace {{ROWS}} with html_rows
+
+    with open("template.html", "r") as f:
+        template = f.read()
+        with open("index.html", "w") as index_f:
+            index_f.write(template.replace("{{ ROWS }}", "\n".join(html_rows))
+    )
+
+
+def dump_summary(_metadata: dict) -> str:
+    rows = []
+
+
+    for commit in _metadata['commits']:
+        message = commit['message'].split("\n")
+        # extract first line from commit message
+        title = message[0]
+        content = "\n".join(message[1:])
+
+        rows.append(f"""
+        <div class="summary">
+            <strong>{title}</strong>
+            <div class="content">
+                {markdown.markdown(content)}
+                <div class="link"><a target="_blank" href="{commit['url']}">View code changes [{commit['id'][:8]}]</a></div>
             </div>
-""")
+        </div>
+        """)
+    return "\n".join(rows)
 
-# dump to index.html file based on template.html, replace {{ROWS}} with html_rows
 
-with open("template.html", "r") as f:
-    template = f.read()
-    with open("index.html", "w") as index_f:
-        index_f.write(template.replace("{{ROWS}}", "\n".join(html_rows))
-)
+if __name__ == "__main__":
+    changelog()
